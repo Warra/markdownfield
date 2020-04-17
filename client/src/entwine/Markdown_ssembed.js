071026b442e5a10ca4ea71e737e996a531d54be6
Warra <warren@indiefin.com>
2020-04-17 14:41:45 +0200

    test getting data

--- client/src/entwine/Markdown_ssembed.js
+++ client/src/entwine/Markdown_ssembed.js
@@ -51,7 +51,8 @@ jQuery.entwine('ss', ($) => {
             const store = window.ss.store;
             const client = window.ss.apolloClient;
             const attrs = this.getOriginalAttributes();
-            console.log(attrs)
+            console.log(args)
+            console.log(store)
             // create/update the react component
             ReactDOM.render(
             <ApolloProvider store={store} client={client}>
