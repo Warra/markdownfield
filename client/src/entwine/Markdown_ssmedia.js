ca6db02eb02853ff6ce108c0e79bbcce506744e6
Warra <warren@indiefin.com>
2020-04-17 10:59:02 +0200

    fix image markdown string

--- client/src/entwine/Markdown_ssmedia.js
+++ client/src/entwine/Markdown_ssmedia.js
@@ -192,11 +192,13 @@ jQuery.entwine('ss', ($) => {
             }
             const data = this.getData();
             const extraData = this.getExtraData();
-            let markdown = '!['
-                + (extraData.CaptionText ? extraData.CaptionText : data.title)
-                + ']([image_link id=' + data.ID +' width=' + data.InsertWidth + ' height=' + data.InsertHeight + ' url=\''+ data.url +'\'] "'
-                + data.title
-                + '")';
+            // let markdown = '!['
+            //     + (extraData.CaptionText ? extraData.CaptionText : data.title)
+            //     + ']([image_link id=' + data.ID +' width=' + data.InsertWidth + ' height=' + data.InsertHeight + ' url=\''+ data.url +'\'] "'
+            //     + data.title
+            //     + '")';
+
+            let markdown = `![${extraData.CaptionText ? extraData.CaptionText : data.title}](${data.url} "${data.title}")${data.ID && `{:#${data.ID}}`}`;
 
             let pos = $field.codemirror.getCursor();
             $field.codemirror.setSelection(pos, pos);
