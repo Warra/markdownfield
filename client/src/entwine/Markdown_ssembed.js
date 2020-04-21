c042e7a9a453050611649657d46b0fec322edb4f
Warra <warren@indiefin.com>
2020-04-21 14:47:42 +0200

    get test data

--- client/src/entwine/Markdown_ssembed.js
+++ client/src/entwine/Markdown_ssembed.js
@@ -137,7 +137,7 @@ jQuery.entwine('ss', ($) => {
             console.log(data);
             console.log(response);
 
-            var shortcode = '[![' + data.Url + '](' + data.PreviewUrl + ')](' + data.Url + ' "' + data.Url + '")';
+            var shortcode = '[![' + response.data.title + '](' + data.PreviewUrl + ')](' + data.Url + ' "' + data.Url + '")';
             
             let pos = $field.codemirror.getCursor();
             $field.codemirror.setSelection(pos, pos);
