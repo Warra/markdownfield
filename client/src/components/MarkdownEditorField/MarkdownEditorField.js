c72c9537fbb12dca050e2abf8ceaf2cecbafa2b7
Warra <warren@indiefin.com>
2020-06-22 15:49:27 +0200

    test

--- client/src/components/MarkdownEditorField/MarkdownEditorField.js
+++ client/src/components/MarkdownEditorField/MarkdownEditorField.js
@@ -97,11 +97,6 @@ class MarkdownEditorField extends React.Component {
                     previewRender: this.previewRender,
                     identifier  : this.props.identifier,
                     showIcons: ["code", "table"],
-                    autosave: {
-                        enabled: true,
-                        uniqueId: `id-${this.id}`,
-                        delay: 300,
-                    },
                     forceSync: true
                 }}
             ></ReactSimpleMDE>
